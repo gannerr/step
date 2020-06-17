ebf6ffa3ed51ece4acf03d5de1e44419cfebbd62
Andrew <agannct@gmail.com>
2020-06-17 19:26:53 +0000

    Modify the greetings into some of my favorite quotes, by myself, about pie.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I like eating pie', 'I like making pie!', 'I like throwing pie！', 'I want a pie!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
