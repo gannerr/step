2a2fc19261ae318af04d067f3176d7f2a918c378
Andrew <agannct@gmail.com>
2020-06-23 22:42:23 +0000

    Introduce parallax scrolling to food page. Create menu-like interface. Food images expand upon click via js.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  expandImg.src = imgs.src;
+  expandImg.parentElement.style.display = "block";
+}
