d2f0505c91105ab8bf269ccbd9ca6ed7ba198851
Andrew <agannct@gmail.com>
2020-06-24 16:13:11 +0000

    Make variable names more specific. Change image size of preview. Scroll to enlarged image when clicked.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function myFunction(imgs) {
+function expandImage(imgs) {
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
+  window.scrollTo(0,document.body.scrollHeight);
 }
